6cd2363f110aca12fc102398eb57330804fd6b7d
DarwinMelaya <melayadarwin@gmail.com>
2026-07-25 13:36:12 +0800

    Add admin pages and update routing structure

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -4,9 +4,10 @@ export { default as LandingPage } from "./landing-page/LandingPage";
 // home page
 export { default as HomePage } from "./home/HomePage";
 
-// short-link redirect
-export { default as RedirectPage } from "./redirect/RedirectPage";
-
 // auth pages
 export { default as Login } from "./auth/Login";
 export { default as SignUp } from "./auth/SignUp";
+
+// admin pages
+export { default as AdminDashboard } from "./admin/AdminDashboard";
+export { default as AdminUser } from "./admin/AdminUser";
--- frontend/src/routers/Routers.tsx
+++ frontend/src/routers/Routers.tsx
@@ -1,15 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LandingPage, HomePage, RedirectPage, Login, SignUp } from "../pages";
+import {
+  LandingPage,
+  HomePage,
+  Login,
+  SignUp,
+  AdminDashboard,
+  AdminUser,
+} from "../pages";
 
 const Routers = () => {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
+        {/* Auth routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/s/:code" element={<RedirectPage />} />
+        {/* Admin routes */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/user" element={<AdminUser />} />
       </Routes>
     </Router>
   );
